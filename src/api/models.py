c496e4916e1a7544e9fcb345752ce4968a896e35
enaguero <enaguero@gmail.com>
2021-07-09 18:03:40 +0000

    Authentication example

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
 
@@ -6,7 +7,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,10 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+    
+    # NOTE: In a real application make sure to properly hash and salt passwords
+    # Sources : https://www.programcreek.com/python/example/92986/werkzeug.security.safe_str_cmp
+    # https://stackoverflow.com/questions/59131067/what-is-the-purpose-of-safe-string-cmp
+    def check_password(self, password_param):
+      return safe_str_cmp(self.password.encode('utf-8'), password_param.encode('utf-8'))
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,52 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/sign_up', methods=['POST'])
+def sign_up_user():
+
+    body_params = request.get_json()
+    print(body_params)
+    name = body_params.get("name", None)
+    last_name = body_params.get("lastName", None)
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+
+    user1 = User(name=name, last_name=last_name, email=email, password=password)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({"msg": "El usuario ha sido creado exitosamente :) :O :O !!!"}), 200
+
+@api.route('/sign_in', methods=['POST'])
+def sign_in_user():
+
+    body_params = request.get_json()
+
+    email = body_params.get("email", None)
+    password = body_params.get("password", None)
+
+    if email == None or password == None:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    user = User.query.filter_by(email=email).one_or_none()
+    print(user.serialize())
+    if not user or not user.check_password(password):
+      return jsonify("Your credentials are wrong, please try again"), 401
+
+    access_token = create_access_token(identity=user.serialize())
+
+    return jsonify({"access_token": access_token}), 200
+
+@api.route("/me", methods=["GET", "PUT"])
+@jwt_required()
+def user_profile():
+  identity = get_jwt_identity()
+  user = current_user(get_jwt_identity())
+
+  return jsonify(user.serialize())
+
+
+def current_user(identity):
+  print(identity["id"])
+  return User.query.get(identity["id"])
